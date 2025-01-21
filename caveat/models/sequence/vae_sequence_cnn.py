89912f4d672268536db80b452d8336f36396e753
Fred Shone <26383933+fredshone@users.noreply.github.com>
2025-01-21 09:52:54 +0000

    alt architectures

--- caveat/models/sequence/vae_sequence_cnn.py
+++ caveat/models/sequence/vae_sequence_cnn.py
@@ -1,19 +1,16 @@
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Tuple, Union
 
-from torch import Tensor, exp, nn
+import torch
+from torch import Tensor, nn
 
-from caveat.models.base import Base
+from caveat.models import Base, CustomDurationEmbedding
 from caveat.models.utils import calc_output_padding, conv2d_size
 
 
-class CondDiscConv(Base):
+class VAESeqCNN(Base):
     def __init__(self, *args, **kwargs):
-        """Convolution based encoder and decoder with encoder embedding layer."""
+        """CNN based encoder and decoder with encoder embedding layer."""
         super().__init__(*args, **kwargs)
-        if self.conditionals_size is None:
-            raise UserWarning(
-                "Model requires conditionals_size, please check you have configures a compatible encoder and condition attributes"
-            )
 
     def build(self, **config):
         hidden_layers = list
@@ -23,19 +20,19 @@ class CondDiscConv(Base):
         stride = Optional[Union[tuple[int, int], int]]
         padding = Optional[Union[tuple[int, int], int]]
 
-        embed_size = config.get("embed_size", self.encodings)
+        encoded_size = self.encodings + 1
         hidden_layers = config["hidden_layers"]
-        latent_dim = 1
+        latent_dim = config["latent_dim"]
         dropout = config.get("dropout", 0)
         kernel_size = config.get("kernel_size", 3)
         stride = config.get("stride", 2)
         padding = config.get("padding", 1)
 
         self.latent_dim = latent_dim
+        # length, _ = self.in_shape
 
         self.encoder = Encoder(
-            input_size=self.encodings,
-            embed_size=embed_size,
+            encoded_size=encoded_size,
             in_shape=self.in_shape,
             hidden_layers=hidden_layers,
             dropout=dropout,
@@ -43,50 +40,23 @@ class CondDiscConv(Base):
             stride=stride,
             padding=padding,
         )
-        self.target_shapes = self.encoder.target_shapes
-        self.flat_size = self.encoder.flat_size
-        self.shape_before_flattening = self.encoder.shape_before_flattening
-        self.encoder = None  # just used encoder to get target shapes :(
-
+        # TODO add drop out to CNNs???
         self.decoder = Decoder(
-            target_shapes=self.target_shapes,
+            encoded_size=encoded_size,
+            target_shapes=self.encoder.target_shapes,
             hidden_layers=hidden_layers,
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
         )
+        self.fc_mu = nn.Linear(self.encoder.flat_size, latent_dim)
+        self.fc_var = nn.Linear(self.encoder.flat_size, latent_dim)
+        self.fc_hidden = nn.Linear(latent_dim, self.encoder.flat_size)
 
-        self.fc_hidden = nn.Linear(self.conditionals_size, self.flat_size)
+        if config.get("share_embed", False):
+            self.decoder.embedding.weight = self.encoder.embedding.weight
 
-    def forward(
-        self,
-        x: Tensor,
-        conditionals: Optional[Tensor] = None,
-        target: Optional[Tensor] = None,
-        **kwargs,
-    ) -> List[Tensor]:
-
-        log_probs = self.decode(z=x, conditionals=conditionals, target=target)
-        return [log_probs, Tensor([]), Tensor([]), Tensor([])]
-
-    def loss_function(
-        self, log_probs: Tensor, target: Tensor, mask: Tensor, **kwargs
-    ) -> dict:
-        """Loss function for discretized encoding [N, L]."""
-        # activity loss
-        recon_act_nlll = self.NLLL(log_probs.squeeze().permute(0, 2, 1), target)
-
-        # loss
-        loss = recon_act_nlll
-
-        return {"loss": loss, "recon_act_nlll_loss": recon_act_nlll}
-
-    def encode(self, input: Tensor):
-        return None
-
-    def decode(
-        self, z: Tensor, conditionals: Tensor, **kwargs
-    ) -> Tuple[Tensor, Tensor]:
+    def decode(self, z: Tensor, target=None, **kwargs) -> Tuple[Tensor, Tensor]:
         """Decode latent sample to batch of output sequences.
 
         Args:
@@ -96,24 +66,16 @@ class CondDiscConv(Base):
             tensor: Output sequence batch [N, steps, acts].
         """
         # initialize hidden state as inputs
-        hidden = self.fc_hidden(conditionals)
-        hidden = hidden.view(self.shape_before_flattening)
+        hidden = self.fc_hidden(z)
+        hidden = hidden.view(self.encoder.shape_before_flattening)
         log_probs = self.decoder(hidden)
         return log_probs
-
-    def predict(
-        self, z: Tensor, conditionals: Tensor, device: int, **kwargs
-    ) -> Tensor:
-        z = z.to(device)
-        conditionals = conditionals.to(device)
-        return exp(self.decode(z=z, conditionals=conditionals, kwargs=kwargs))
 
 
 class Encoder(nn.Module):
     def __init__(
         self,
-        input_size: int,
-        embed_size: int,
+        encoded_size: int,
         in_shape: tuple,
         hidden_layers: list,
         dropout: float = 0.1,
@@ -124,6 +86,7 @@ class Encoder(nn.Module):
         """2d Convolutions Encoder.
 
         Args:
+            encoded_size (int): number of encoding classes and hidden size.
             in_shape (tuple[int, int, int]): [C, time_step, activity_encoding].
             hidden_layers (list, optional): _description_. Defaults to None.
             dropout (float): dropout. Defaults to 0.1.
@@ -133,8 +96,10 @@ class Encoder(nn.Module):
         """
         super(Encoder, self).__init__()
         h = in_shape[0]
-        self.embedding = nn.Embedding(input_size, embed_size)
-        w = embed_size
+        self.embedding = CustomDurationEmbedding(
+            encoded_size, encoded_size, dropout=dropout
+        )
+        w = encoded_size
         channels = 1
 
         modules = []
@@ -167,11 +132,19 @@ class Encoder(nn.Module):
         self.encoder = nn.Sequential(*modules)
         self.flat_size = int(channels * h * w)
 
+    def forward(self, x):
+        y = self.dropout(self.embedding(x.int()))
+        y = y.unsqueeze(1)  # add channel dim for Conv
+        y = self.encoder(y)
+        y = y.flatten(start_dim=1)
+        return y
+
 
 class Decoder(nn.Module):
     def __init__(
         self,
-        target_shapes,
+        encoded_size: int,
+        target_shapes: list,
         hidden_layers: list,
         kernel_size: Union[tuple[int, int], int] = 3,
         stride: Union[tuple[int, int], int] = 2,
@@ -187,6 +160,7 @@ class Decoder(nn.Module):
             padding (Union[tuple[int, int], int], optional): _description_. Defaults to 1.
         """
         super(Decoder, self).__init__()
+        self.hidden_size = encoded_size
         modules = []
         target_shapes.reverse()
 
@@ -221,14 +195,22 @@ class Decoder(nn.Module):
                     output_padding=calc_output_padding(target_shapes[-1]),
                 ),
                 nn.BatchNorm2d(target_shapes[-1][0]),
-                nn.Tanh(),
             )
         )
 
         self.decoder = nn.Sequential(*modules)
         self.logprob_activation = nn.LogSoftmax(dim=-1)
+        self.duration_activation = nn.Sigmoid()
 
     def forward(self, hidden, **kwargs):
         y = self.decoder(hidden)
         y = y.squeeze(1)  # remove conv channel dim
-        return self.logprob_activation(y)
+        acts_logits, durations = torch.split(
+            y, [self.hidden_size - 1, 1], dim=-1
+        )
+        acts_log_probs = self.logprob_activation(acts_logits)
+        durations = self.duration_activation(durations)
+        durations = torch.log(durations)
+        log_prob_outputs = torch.cat((acts_log_probs, durations), dim=-1)
+
+        return log_prob_outputs
